64c6ad35d9584d4abdfb3c04b005a7fc4777413a
brunooomelo <bruno94@outlook.com>
2023-01-16 12:40:50 -0300

    add 10 itens to mint

--- src/server/context.ts
+++ src/server/context.ts
@@ -1,17 +1,21 @@
-import path from "path";
+import { v4 as uuid } from "uuid";
 import { inferAsyncReturnType } from "@trpc/server";
 import { InMemoryDatabase } from "in-memory-database";
 
 const db = new InMemoryDatabase();
-db.set("0f29c741-f083-4805-8cc4-13c73a05e803", {
-  id: "0f29c741-f083-4805-8cc4-13c73a05e803",
-  name: "NFT #1",
-  url: "https://pbs.twimg.com/media/Fkj1j1zVEAAOpeI?format=jpg&name=large",
-  value: "0.01",
-  owner: "",
-  transactionHash: "",
-  createdBy: "0x9939DD143F8055518d09c7BC479B16e7D537204E",
+Array.from({ length: 10 }).map((_, index) => {
+  const id = uuid();
+  db.set("0f29c741-f083-4805-8cc4-13c73a05e803", {
+    id,
+    name: `NFT #${index + 1}`,
+    url: "https://pbs.twimg.com/media/Fkj1j1zVEAAOpeI?format=jpg&name=large",
+    value: "1.00",
+    owner: "",
+    transactionHash: "",
+    createdBy: "0x9939DD143F8055518d09c7BC479B16e7D537204E",
+  });
 });
+
 export async function createContext() {
   return {
     db,
